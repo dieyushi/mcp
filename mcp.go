cf491e70da12e19c3c780df72d1013df1f23e6bc
dieyushi <zzscu7@gmail.com>
2012-09-16 15:29:57 +0800

    add runtime.GOMAXPROCS setting

--- mcp.go
+++ mcp.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/monnand/goredis"
+	"runtime"
 )
 
 var redisClient goredis.Client
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU() + 1)
 	redisClient.Addr = ":6379"
 	var c chan int
 	go webmain()
