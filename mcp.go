8a5384661250846955545de506bbdbfe71204bb4
dieyushi <zzscu7@gmail.com>
2012-09-16 22:19:13 +0800

    add running in backgroup function,with some issue
    we can run ./mcp -d to make it running in the backgroup, when there has a
    instance, run ./mcp will mention you error happens, while ./mcp -d will not.

--- mcp.go
+++ mcp.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"github.com/monnand/goredis"
+	"log"
+	"os"
+	"os/exec"
 	"runtime"
+	"strings"
 )
 
 var redisClient goredis.Client
 var mainChan = make(chan bool)
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU() + 1)
-	redisClient.Addr = ":6379"
+	d := flag.Bool("d", false, "Whether or not to launch in the background(like a daemon)")
+	flag.Parse()
 
-	go webmain()
-	go servermain()
+	println("trying to listen on port 8080 and 44444")
 
-	<-mainChan
-	<-mainChan
+	if *d {
+		cmd := exec.Command(os.Args[0])
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		cmd.Start()
+		if strings.Contains(out.String(), "error") {
+			log.Printf("error: %v", out.String())
+			cmd.Process.Kill()
+		} else {
+			cmd.Process.Release()
+			println("Serving in the background")
+		}
+	} else {
+		runtime.GOMAXPROCS(runtime.NumCPU() + 1)
+		redisClient.Addr = ":6379"
+
+		go webmain()
+		go servermain()
+
+		<-mainChan
+		<-mainChan
+	}
 }
